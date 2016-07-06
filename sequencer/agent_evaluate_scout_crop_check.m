e62cabafac2dd093dc42af22f697979fe3b94b91
Evan Roche <eroche456@gmail.com>
2016-07-06 16:54:55 -0700

    modification for agent_evaluate function

--- sequencer/agent_evaluate_scout_crop_check.m
+++ sequencer/agent_evaluate_scout_crop_check.m
@@ -1,87 +1,45 @@
-function [agent_pool,d] = agent_evaluate_scout( agent_pool, agent_index, im, im_label, d, p, workspace )     
-    global cnn_svm_model dog_svm_model walker_svm_model leash_svm_model cnn_net cnn_layer sub_counts;
-    interest = agent_pool(agent_index).interest;
-    
-    % figure out which distribution index is associated with our current interest
-    di = find(strcmp({d.interest},interest)); 
-    [d(di), sampled_box_r0rfc0cf,w,h,rc,cc] = situate_sample_box( d(di), p );
-
-    % adjust sampled box to fit within the image bounds
-    %   the sampled location will already be inhibited by the
-    %   situate_sample_box call, so all we need to do is adjust the box
-    %   bounds for display, IOU calculation, and sending to the classifier
-        r0 = max(sampled_box_r0rfc0cf(1), 1);
-        rf = min(sampled_box_r0rfc0cf(2), d(di).image_size(1) );
-        c0 = max(sampled_box_r0rfc0cf(3), 1);
-        cf = min(sampled_box_r0rfc0cf(4), d(di).image_size(2) );
-        r0 = round(r0);
-        rf = round(rf);
-        c0 = round(c0);
-        cf = round(cf);
-        sampled_box_r0rfc0cf = [r0 rf c0 cf];
-        w = cf-c0+1;
-        h = rf-r0+1;
-        rc = r0 + h/2;
-        cc = c0 + h/2;
-    
-    agent_pool(agent_index).theta{3} = sampled_box_r0rfc0cf;    % sampled in [r0,rf,c0,cf]
-    agent_pool(agent_index).theta{2} = [w,h];                   % the sampled box width/height
-    agent_pool(agent_index).theta{1} = [rc,cc];                 % the center point of the box [row,column]
-  
-    % figure out the internal support
-
-        switch interest
-            case 'dog'
-                switch p.classification_method
-                    case 'HOG-SVM'
-                        model_ind = find( strcmp( 'dog', {p.classifier.data.target_label} ) );
-                        [~,internal_support] = p.classifier.apply( im(r0:rf,c0:cf,:), p.classifier.data(model_ind) );
-                    case 'IOU-oracle'
-                        agent_box_xywh      = [ c0 r0 w h ];
-                        intersection_scores = intersection_over_union_xywh( agent_box_xywh, im_label.boxes );
-                        % limit to 2 sig figs so behavior matches display
-                        intersection_scores = round(intersection_scores * 100) / 100;
-                        internal_support = intersection_scores(im_label.is_dog);
-                        internal_support = internal_support(1);% in case there are several, which there should not be, just use the first
-                        orientation = im_label.orientations{im_label.is_dog};
-                    otherwise
-                        error('unrecognized p.classification_method');
-                end
-                
-            case 'person'
-                switch p.classification_method
-                    case 'HOG-SVM'
-                        model_ind = find( strcmp( 'person', {p.classifier.data.target_label} ) );
-                        [~,internal_support] = p.classifier.apply( im(r0:rf,c0:cf,:), p.classifier.data(model_ind) );
-                    case 'IOU-oracle'
-                        agent_box_xywh      = [ c0 r0 w h ];
-                        intersection_scores = intersection_over_union_xywh( agent_box_xywh, im_label.boxes );
-                        % limit to 2 sig figs so behavior matches display
-                        intersection_scores = round(intersection_scores * 100) / 100;
-                        internal_support = intersection_scores(im_label.is_ped);
-                        internal_support = internal_support(1);% in case there are several, which there should not be, just use the first
-                        orientation = im_label.orientations{im_label.is_ped};
-                    otherwise
-                        error('unrecognized p.classification_method');
-                end
-                
-            case 'leash'
-                switch p.classification_method
-                    case 'HOG-SVM'
-                        model_ind = find( strcmp( 'leash', {p.classifier.data.target_label} ) );
-                        [~,internal_support] = p.classifier.apply( im(r0:rf,c0:cf,:), p.classifier.data(model_ind) );
-                    case 'IOU-oracle'
-                        agent_box_xywh      = [ c0 r0 w h ];
-                        intersection_scores = intersection_over_union_xywh( agent_box_xywh, im_label.boxes );
-                        % limit to 2 sig figs so behavior matches display
-                        intersection_scores = round(intersection_scores * 100) / 100;
-                        internal_support = intersection_scores(im_label.is_leash);
-                        internal_support = internal_support(1);% in case there are several, which there should not be, just use the first
-                        orientation = im_label.orientations{im_label.is_leash};
-                    otherwise
-                        error('unrecognized p.classification_method');
-                end
-                
-            otherwise
-                error('situate_sketch:interest_label_match_failed','agent has an unrecognized interest');
+global cnn_svm_model dog_svm_model walker_svm_model leash_svm_model cnn_net cnn_layer sub_counts;
+if internal_support > p.internal_support_threshold
+        if strcmp(interest,'person') == 1
+            crop_object_class = 'walker';
+        else
+            crop_object_class = interest;
         end
+        
+        if strcmp(crop_object_class,'dog') == 1
+            cnn_svm_model = dog_svm_model;
+        elseif strcmp(crop_object_class,'walker') == 1
+            cnn_svm_model = walker_svm_model;
+        elseif strcmp(crop_object_class,'leash') == 1
+            cnn_svm_model = leash_svm_model;
+        end
+        crop_start = agent_box_xywh;
+        [new_guess_crop,new_guess_iou,new_guess_moves] = crop_check(hf,im,crop_object_class,im_label,...
+            crop_start,6,internal_support);
+
+        if new_guess_iou > internal_support
+            bounding_box = new_guess_crop
+            iou_score = new_guess_iou;
+            disp('new guess wins')
+            best_guess = 'new';
+        else 
+            bounding_box = agent_pool(agent_index).theta{3};
+            iou_score = agent_pool(agent_index).theta{4}
+            disp('original guess wins')
+            best_guess = 'old';
+        end 
+        move_record = {best_guess,new_guess_moves}
+        sub_counts = [sub_counts; move_record];
+        save('/u/eroche/matlab/sub_counts.mat','sub_counts');
+        agent_pool(end+1).type   = 'reviewer';
+        agent_pool(end).interest = agent_pool(agent_index).interest;
+        agent_pool(end).urgency  = p.agent_urgency_defaults.reviewer;
+        agent_pool(end).theta{1} = iou_score; % internal support from calling scout
+        agent_pool(end).theta{3} = bounding_box; % box bounds from calling scout
+        agent_pool(end).theta{4} = interest;
+
+        % agent_pool(end).theta{5} = orientation;
+        % in the future, more info from the scout will probably need to
+        % be passed along to the reviewer for the review process to be
+        % reasonable (info like where the scout was looking)
+    end
